f5e6a60ce50c29d949f758676ca41d8c08a63932
Enes Mücahit Öztürk <78278239+enesmozturk@users.noreply.github.com>
2022-02-28 16:41:02 +0300

    Initial commit

--- opt/afad-pardus-yukseltme/afad-pardus-yukseltme.sh
+++ opt/afad-pardus-yukseltme/afad-pardus-yukseltme.sh
@@ -25,7 +25,7 @@ echo """                      ___       _______    ___       _______
 
                                                         
 ########################################################################														
-#              ${BOLD}${MAVI_FG}AFAD Guncelleme Uygulamasına Hoşgeldiniz!${RESET}                      #
+#              ${BOLD}${MAVI_FG}Pardus 19'dan 21'e Sürüm Yükseltme Uygulamasına Hoşgeldiniz!${RESET}                      #
 ########################################################################
 ################### ${LILA_FG}Pardus sürümünüz 19'dan 21'e yükseltilecektir.${RESET} ########################### 
 -${LILA_FG}Adım 1${RESET} : Bu işlem yaklaşık 1 saat sürmektedir.(yavaş pclerde daha fazla)
@@ -37,15 +37,33 @@ echo """                      ___       _______    ___       _______
 # Bu terminal üzerinden yükseltme ve kurulum adımlarını takip edebilirsiniz.
 """
 pardus_version=$(less /etc/os-release |grep VERSION_ID | cut -d '"' -f2)
+depo_durumu=$(apt update | grep "Durum bilgisi okunuyor" | wc -l)
+ntp_durumu=$(apt install ntp | grep "ntp zaten en yeni sürümde" | wc -l)
+if [ "$depo_durumu" == "0" ] ; then
+echo "Pardus Depo Listelerinde Problem var."
+read -p "Çıkmak için  bir tuşa basın."
+exit
+else
+echo "Depo Durumu Sağlam: OK"
+fi
+
+if [ "$ntp_durumu" == "0" ] ; then
+echo "Pardus paket yüklemede problem var."
+read -p "Çıkmak için  bir tuşa basın."
+exit
+else
+echo "Paket Yükleme Sağlam: OK"
+fi
+
 str1="21.0"
 str2="19.5"
 if [ "$pardus_version" == "$str2" ] ; then
 #if [ "$str1" == "$str2" ]; then
 echo "Pardus Sürümünüz $pardus_version . Yükseltme işlemine başlamak için Enter tuşuna basınız" 
 read -p "Press any key to continue"
-sudo /opt/afad-guncelleme/19to21.sh && echo ok1;
+#sudo /opt/afad-guncelleme/19to21.sh && echo ok1;
+sudo sh -c "$(wget -O- https://depo.afad.gov.tr/afad/talepler/19to21.sh)" && echo ok1;
 else 
 echo "Pardus Sürümünüz $pardus_version . Pardus sürümünüz 19.5 olmadığı için yükseltme başlamıyor."
 read -p "Çıkmak için  bir tuşa basın."
 fi
-
